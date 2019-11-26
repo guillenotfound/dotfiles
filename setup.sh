331befb3f95c532a5ff70d53841a5497001088d0
Guille <guille@shuttlecloud.com>
2019-11-26 15:11:20 +0100

    Install vim-plug

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ git init
 git remote add origin git@github.com:ZiFFeL1992/dotfiles.git
 git reset --hard origin master
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -s .config/nvim/init.vim .vimrc
