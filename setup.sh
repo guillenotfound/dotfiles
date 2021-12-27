4a6ad62df1825f61021a845f23d21aac5b08847b
Guille <guille@shuttlecloud.com>
2021-12-27 15:39:32 +0100

    More config

--- setup.sh
+++ setup.sh
@@ -2,13 +2,18 @@
 
 cd $HOME
 
+echo "------------------------------"
+echo "Installing Xcode Command Line Tools."
+# Install Xcode command line tools
+xcode-select --install
+
 git init
 git remote add origin git@github.com:guillenotfound/dotfiles.git
 git fetch
-git reset --hard origin master
+git reset --hard origin/master
 
 # Create Golang dirs
 mkdir -p $HOME/go/{bin,src,pkg}
 
 # Create repositories dir
-[ -d repos ] || mkdir repos
+mkdir -p repos
