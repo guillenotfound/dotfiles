6dc86c2ab193bca2b346a3d65b934f1f527c9b9b
Guille <guillenotfound@gmail.com>
2023-01-25 02:20:26 +0100

    feat: polish tmux config, tt script

---
+++ scripts/tt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ZOXIDE_RESULT=$(zoxide query -l | fzf --reverse)
+
+if [ -z "$ZOXIDE_RESULT" ]; then
+  exit 0
+fi
+
+FOLDER=$(basename $ZOXIDE_RESULT)
+SESSION_NAME=$(echo $FOLDER | tr ' ' '_' | tr '.' '_')
+
+# lookup tmux session name
+SESSION=$(tmux list-sessions | grep -f $SESSION_NAME | awk '{print $1}')
+SESSION=${SESSION//:/}
+
+if [ -z "$SESSION" ]; then
+  # session does not exist
+  # jump to directory
+  cd "$ZOXIDE_RESULT"
+  # create session
+  tmux new-session -d -s $SESSION_NAME
+  # attach to session
+  tmux switch-client -t $SESSION_NAME
+else
+  tmux switch-client -t $SESSION
+fi
