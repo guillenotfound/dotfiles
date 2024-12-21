85f89aec51c973399f8eea41f3bcc11017944271
Guille <guillenotfound@gmail.com>
2024-12-21 04:00:20 +0100

    update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,66 +1,106 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 
-config.font = wezterm.font 'MesloLGLDZ Nerd Font Mono'
+config.font = wezterm.font("MesloLGLDZ Nerd Font Mono")
+config.font_size = 13
+
+config.max_fps = 120
 
 -- config.leader = { key = '`', timeout_milliseconds = 1000 }
+-- TODO: missing command left/right
 config.keys = {
-  -- Sends ESC + b and ESC + f sequence, which is used
-  -- for telling your shell to jump back/forward.
-  {
-    -- When the left arrow is pressed
-    key = 'LeftArrow',
-    -- With the "Option" key modifier held down
-    mods = 'OPT',
-    -- Perform this action, in this case - sending ESC + B
-    -- to the terminal
-    action = wezterm.action.SendKey {
-      key = 'b',
-      mods = 'ALT',
-    },
-  },
-  {
-    key = 'RightArrow',
-    mods = 'OPT',
-    action = wezterm.action.SendKey {
-      key = 'f',
-      mods = 'ALT',
-    },
-  },
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "b",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "f",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = act.SendKey({
+			key = "a",
+			mods = "CTRL",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = act.SendKey({
+			key = "e",
+			mods = "CTRL",
+		}),
+	},
+	{
+		key = "Backspace",
+		mods = "CMD",
+		action = act.SendKey({
+			key = "u",
+			mods = "CTRL",
+		}),
+	},
 
-  {
-    key = ',',
-    mods = 'SUPER',
-    action = wezterm.action.SpawnCommandInNewTab {
-      cwd = wezterm.home_dir,
-      args = { 'nvim', wezterm.config_file },
-    },
-  },
+	{
+		key = ",",
+		mods = "SUPER",
+		action = act.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+			args = { "nvim", wezterm.config_file },
+		}),
+	},
 
-  { key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }) },
-  { key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }) },
+	-- { key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	-- { key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
-  {
-    key = 'Enter',
-    mods = 'CMD',
-    action = wezterm.action.ToggleFullScreen
-  },
+	{
+		key = "Enter",
+		mods = "CMD",
+		action = act.ToggleFullScreen, -- Uses rectangle
+	},
+
+	{
+		key = "p",
+		mods = "CMD",
+		action = act.Multiple({
+			act.SendKey({ key = "`" }),
+			act.SendKey({ key = "p", mods = "CTRL" }),
+		}),
+	},
+
+	{
+		key = "j",
+		mods = "CMD",
+		action = act.Multiple({
+			act.SendKey({ key = "`" }),
+			act.SendKey({ key = "j", mods = "CTRL" }),
+		}),
+	},
 }
 
 config.set_environment_variables = {
-  PATH = '/opt/homebrew/bin:' .. os.getenv('PATH')
+	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
 }
 
 config.enable_tab_bar = false
--- config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
--- and finally, return the configuration to wezterm
+config.window_close_confirmation = "NeverPrompt"
+
 return config
