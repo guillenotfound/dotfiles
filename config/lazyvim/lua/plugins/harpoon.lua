0b6d7f940fbc235e517597dba85f6c27f28f49b8
Guille <guillenotfound@gmail.com>
2025-01-16 04:06:22 +0100

    refactor(harpoon): config

--- config/lazyvim/lua/plugins/harpoon.lua
+++ config/lazyvim/lua/plugins/harpoon.lua
@@ -2,6 +2,9 @@ return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
   opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
     settings = {
       save_on_toggle = true,
     },
@@ -22,7 +25,6 @@ return {
           title = "Harpoon btw",
           title_pos = "center",
           border = "rounded",
-          ui_width_ratio = 0.40,
         })
       end,
       desc = "Harpoon Quick Menu",
@@ -42,46 +44,32 @@ return {
       desc = "Harpoon Go to next buffer",
     },
     {
-      "<A-q>",
+      "<A-u>",
       function()
         require("harpoon"):list():select(1)
       end,
       desc = "Harpoon Go to 1st buffer",
     },
     {
-      "<A-w>",
+      "<A-i>",
       function()
         require("harpoon"):list():select(2)
       end,
       desc = "Harpoon Go to 2st buffer",
     },
     {
-      "<A-e>",
+      "<A-o>",
       function()
         require("harpoon"):list():select(3)
       end,
       desc = "Harpoon Go to 3st buffer",
     },
     {
-      "<A-r>",
+      "<A-p>",
       function()
         require("harpoon"):list():select(4)
       end,
       desc = "Harpoon Go to 4st buffer",
-    },
-    {
-      "<A-t>",
-      function()
-        require("harpoon"):list():select(5)
-      end,
-      desc = "Harpoon Go to 5st buffer",
-    },
-    {
-      "<A-y>",
-      function()
-        require("harpoon"):list():select(6)
-      end,
-      desc = "Harpoon Go to 6st buffer",
     },
   },
 }
