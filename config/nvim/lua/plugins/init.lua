8fa9d9f62022eb794f6ccb697d6a4d2852e65229
Guille <guillenotfound@gmail.com>
2025-04-24 01:57:08 +0200

    feat(nvim): add diffview plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -137,6 +137,12 @@ return {
 
   {
     "lewis6991/gitsigns.nvim",
+    dependencies = {
+      {
+        "sindrets/diffview.nvim",
+        config = true,
+      },
+    },
     opts = {
       preview_config = {
         border = "rounded",
