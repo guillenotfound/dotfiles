ea5013945e114ed2a314434c15f921a4a4e0573b
Guille <guillenotfound@gmail.com>
2025-07-19 23:35:53 +0200

    feat(nvim): disable changing dir with helm_ls

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,18 @@
+-- Disable diagnostics for `.env` files
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = ".env*",
   callback = function()
     vim.diagnostic.enable(false)
+  end,
+})
+
+-- Prevent `helm_ls` to change cwd.
+vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost" }, {
+  pattern = { ".helm", "Chart.yaml", "values.yaml" },
+  callback = function()
+    local root = require("lazyvim.util").root.get()
+    if root and vim.fn.getcwd() ~= root then
+      vim.cmd("cd " .. root)
+    end
   end,
 })
