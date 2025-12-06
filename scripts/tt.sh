f16da24b4a9e9099475c241cd8258a728b9f0556
Guille <guillenotfound@gmail.com>
2025-12-06 16:50:02 +0100

    refactor(tt.sh): linting

--- scripts/tt.sh
+++ scripts/tt.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-ZOXIDE_RESULT=`zoxide query -l | fzf --reverse`
+ZOXIDE_RESULT=$(zoxide query -l | fzf --reverse)
 
 if [ -z "$ZOXIDE_RESULT" ]; then
   exit 0
 fi
 
-FOLDER=$(basename $ZOXIDE_RESULT)
-SESSION_NAME=$(echo $FOLDER | tr ' ' '_' | tr '.' '_')
+FOLDER=$(basename "$ZOXIDE_RESULT")
+SESSION_NAME=$(echo "$FOLDER" | tr ' ' '_' | tr '.' '_')
 
 # lookup tmux session name
-SESSION=$(tmux list-sessions | grep -F $SESSION_NAME | awk '{print $1}')
+SESSION=$(tmux list-sessions | grep -F "$SESSION_NAME" | awk '{print $1}')
 SESSION=${SESSION//:/}
 
 if [ -z "$SESSION" ]; then
   # session does not exist
   # jump to directory
-  cd "$ZOXIDE_RESULT"
+  cd "$ZOXIDE_RESULT" || exit
   # create session
-  tmux new-session -d -s $SESSION_NAME
+  tmux new-session -d -s "$SESSION_NAME"
   # attach to session
-  tmux switch-client -t $SESSION_NAME
+  tmux switch-client -t "$SESSION_NAME"
 else
-  tmux switch-client -t $SESSION
+  tmux switch-client -t "$SESSION"
 fi
