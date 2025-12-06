eac221b5f8bb3f28a8e7a54d0d19cbf93183cec9
Guille <guillenotfound@gmail.com>
2025-12-06 19:09:45 +0100

    chore: update nvim config

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -9,5 +9,20 @@ return {
         vim.g.fubitive_domain_pattern = os.getenv("BITBUCKET_ENDPOINT")
       end,
     },
+
+    {
+      "shumphrey/fugitive-gitlab.vim",
+      enabled = function()
+        if os.getenv("GITLAB_SSH") then
+          return true
+        else
+          return false
+        end
+      end,
+      config = function()
+        vim.g.fugitive_gitlab_domains = { [os.getenv("GITLAB_SSH")] = os.getenv("GITLAB_PORTAL") }
+        vim.g.gitlab_api_keys = { [os.getenv("GITLAB_PORTAL")] = os.getenv("GITLAB_PAT") }
+      end,
+    },
   },
 }
