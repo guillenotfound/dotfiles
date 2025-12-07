a2911b8f7e3a00bf523360d004d9b121fcdfd893
Guille <guillenotfound@gmail.com>
2025-12-08 00:53:21 +0100

    chore: enable explorer extra

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -41,6 +41,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
 
     { import = "lazyvim.plugins.extras.util.dot" },
+    { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
 
     -- import/override with your plugins
     { import = "plugins" },
